40406b30dc46d29447782bb84ddd4f00ada28824
hardcorehiker <hardcorehiker@gmail.com>
2016-10-27 10:35:36 -0500

    updated footer.php
    updated the footer year to 2016

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -1,16 +1,21 @@
-                        </div>  <!-- Content ends -->   
-                    </div> <!-- Wrapper Ends -->
-                
-                <footer>    <!-- Footer -->
-                    <div class="row">
-                        <div class="col-md-3 copyright">&copy; 2015 Infini Consulting</div>
-                        <div class="col-md-6 owned">This Portal is owned and maintained by Infini Consulting.</div>
-                        <div class="col-md-3 policy"><p><a href="https://www.theinfini.com/PrivacyPolicy.pdf" target="_blank">Privacy Policy</a></p></div>
-                    </div>
-                </footer>   <!-- Footer // -->
-</div> <!-- Container Ends -->
+            </div>  <!-- Main Container // --> 
+        <footer>    <!-- Footer -->
+                <a href="https://www.theinfini.com" target="_blank">
+                    <img style="float: left; margin-right: 10px; margin-top: -5px;" src="<?php echo $linkToALL;?>/assets/images/infini.svg" alt="Infini" width="100"/>
+                </a>
+                    <!--<span class="owned">This Portal is owned and maintained by Infini Consulting.</span>  <br/><br/>
+                    <span class="emailContact">Please <a href="<?php echo $linkToALL; ?>/contact.php">contact us</a> if you have any questions or concerns about the Portal.</span>-->
+            </div>  <!-- Copyright //-->
+            <div id="policyFooter"> <!-- Policy -->
+                <a href="https://www.theinfini.com/PrivacyPolicy.pdf" target="_blank">Privacy Policy</a><br/>
+                <a href="<?php echo $linkToALL; ?>/contact.php">Contact Us</a>
+            </div>  <!-- Policy // -->
+<br/>            
+<span style="clear:both";>&nbsp;&copy; 2016 Infini Consulting</span>
+        </footer>   <!-- Footer // -->
 
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="<?php echo $linkToALL;?>/assets/js/scriptFooter.js"></script>
+        <script type="text/javascript" src="<?php echo $linkToALL;?>/assets/plugins/WOWSlider/engine1/wowslider.js"></script>
+        <script type="text/javascript" src="<?php echo $linkToALL;?>/assets/plugins/WOWSlider/engine1/script.js"></script>
     </body> <!-- Body // -->
 </html>
